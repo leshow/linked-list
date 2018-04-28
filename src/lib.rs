94a6a4ad251f4b9cf8a2ff0af364ff4290ef6f62
Evan Cameron <cameron.evan@gmail.com>
2018-04-28 15:07:12 -0400

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,6 @@ pub struct List<'a, T: 'a> {
     marker: PhantomData<&'a Node<T>>,
 }
 
-type Link<T> = Option<Box<Node<T>>>;
-
 pub struct Node<T> {
     elem: T,
     next: Option<NonNull<Node<T>>>,
@@ -60,28 +58,20 @@ mod test {
     #[test]
     fn basics() {
         let mut list = List::new();
-
-        // Check empty list behaves right
         assert_eq!(list.pop(), None);
 
-        // Populate list
         list.push(1);
         list.push(2);
         list.push(3);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
 
-        // Push some more just to make sure nothing's corrupted
         list.push(4);
         list.push(5);
 
-        // Check normal removal
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(4));
-
-        // Check exhaustion
         assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), None);
     }
